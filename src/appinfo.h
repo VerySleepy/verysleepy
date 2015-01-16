347c6c4a79bbb2c7a75c90be26759572184698d0
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-01-16 19:13:11 +0000

    Revert name and URL change

--- src/appinfo.h
+++ src/appinfo.h
@@ -1,11 +1,11 @@
-#define APPNAME "Very Sleepy CS"
+#define APPNAME "Very Sleepy"
 
 #define VERSION "0.91"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 91
 
-#define VENDOR  "thecybershadow.net"
+#define VENDOR  "codersnotes.com"
 
-#define APPURL  "https://github.com/CyberShadow/verysleepy/"
+#define APPURL  "http://www.codersnotes.com/sleepy/sleepy"
 
 #define FORMAT_VERSION "0.90"
