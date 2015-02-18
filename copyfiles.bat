4f7ffc9ceccc336a3b78e574c1bf792349479958
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-02-18 09:58:21 +0000

    copyfiles.bat: Copy over debug versions of Wine DbgHelp

--- copyfiles.bat
+++ copyfiles.bat
@@ -7,7 +7,7 @@ copy /y dbghelp_%PLATFORM%\*.* %DEST%
 
 if %PLATFORM%==x86 set PLATFORM=Win32
 
-copy /y "src\dbghelpw\wine\dlls\dbghelp\vs\bin\%PLATFORM%\Release\dbghelpw.dll" %DEST%
-copy /y "src\dbghelpw\wine\dlls\dbghelp\vs\bin\%PLATFORM%\Release - Wow64\dbghelpw.dll" %DEST%\dbghelpw_wow64.dll
+copy /y "src\dbghelpw\wine\dlls\dbghelp\vs\bin\%PLATFORM%\Debug\dbghelpw.dll" %DEST%
+copy /y "src\dbghelpw\wine\dlls\dbghelp\vs\bin\%PLATFORM%\Debug - Wow64\dbghelpw.dll" %DEST%\dbghelpw_wow64.dll
 
 copy /y src\crashback\bin\%PLATFORM%\Release\crashreport.exe %DEST%
