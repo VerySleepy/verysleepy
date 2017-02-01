d26cb82a75cf139d389218841d628c27939a439a
Vladimir Panteleev <vladimir@thecybershadow.net>
2017-02-01 07:11:57 +0000

    copyfiles.bat: Add error handling

--- src/copyfiles.bat
+++ src/copyfiles.bat
@@ -14,16 +14,21 @@ if %PLATFORM%==x64   set PLATFORM_X=x64
 
 set DBGHELPERS=dbghelp_%PLATFORM_X%
 copy /y %DBGHELPERS%\*.* %DEST%
+if errorlevel 1 exit /b 1
 
-if not exist %DBGHELPERS%\dbghelpw.dll copy /y "thirdparty\wine\dlls\dbghelp\vs\bin\%PLATFORM%\%CONFIGURATION%\dbghelpw.dll" %DEST%
+                   if not exist %DBGHELPERS%\dbghelpw.dll       copy /y "thirdparty\wine\dlls\dbghelp\vs\bin\%PLATFORM%\%CONFIGURATION%\dbghelpw.dll"         %DEST%
+if errorlevel 1 exit /b 1
 if %PLATFORM%==x64 if not exist %DBGHELPERS%\dbghelpw_wow64.dll copy /y "thirdparty\wine\dlls\dbghelp\vs\bin\%PLATFORM%\%CONFIGURATION% - Wow64\dbghelpw.dll" %DEST%\dbghelpw_wow64.dll
+if errorlevel 1 exit /b 1
 
 if %PLATFORM%==Win32 set PLATFORM_BITS=32
 if %PLATFORM%==x64   set PLATFORM_BITS=64
 
 if not exist %DBGHELPERS%\dbghelpdr.dll copy thirdparty\drmingw_build_%PLATFORM_BITS%\bin\mgwhelp.dll %DEST%\dbghelpdr.dll
+if errorlevel 1 exit /b 1
 
 if not defined VCINSTALLDIR if defined VS100COMNTOOLS set VCINSTALLDIR=%VS100COMNTOOLS%\..\..\VC
 for %%D in (msvcr100.dll msvcp100.dll) do copy "%VCINSTALLDIR%\redist\%PLATFORM_X%\Microsoft.VC100.CRT\%%D" %DEST%\%%D
 
 copy /y src\crashback\bin\%PLATFORM%\%CONFIGURATION%\crashreport.exe %DEST%
+if errorlevel 1 exit /b 1
